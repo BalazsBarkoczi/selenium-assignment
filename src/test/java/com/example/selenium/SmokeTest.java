85cf71203ca4de67e1cad6e8a66b7bc354d28c05
BalazsBarkoczi <balazs.barkoczi121@gmail.com>
2026-05-09 17:37:44 +0200

    feat: Base Project

---
+++ src/test/java/com/example/selenium/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.example.selenium;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void projectStarts() {
+        Assertions.assertTrue(true);
+    }
+}
