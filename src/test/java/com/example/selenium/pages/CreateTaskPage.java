dffe06c477eaf2b8d8e1739111bd324e30e559fc
BalazsBarkoczi <balazs.barkoczi121@gmail.com>
2026-05-10 18:43:07 +0200

    feat: Enhance CreateTaskPage with input field handling

--- src/test/java/com/example/selenium/pages/CreateTaskPage.java
+++ src/test/java/com/example/selenium/pages/CreateTaskPage.java
@@ -11,10 +11,16 @@ public class CreateTaskPage extends BasePage {
         super(driver);
     }
 
+    private static final By RATE_INPUT = By.xpath("//label[normalize-space()='Rate']/following::input[@type='text'][1] | //section[.//label[normalize-space()='Rate']]//input[@type='text'][1]");
+    private static final By TASK_NUMBER_INPUT = By.xpath("//label[normalize-space()='Task Number']/following::input[@type='text'][1] | //section[.//label[normalize-space()='Task Number']]//input[@type='text'][1]");
+    private static final By DESCRIPTION_INPUT = By.xpath("//label[normalize-space()='Description']/following::textarea[1] | //section[.//label[normalize-space()='Description']]//textarea[1]");
     private static final By SAVE_BUTTON = By.xpath("//button[normalize-space(text())='Save']");
     private static final By SUCCESS_TOAST = By.xpath("//*[contains(text(), 'Successfully created task')]");
 
     public void fillTaskForm(){
+        typeText(RATE_INPUT, "12345");
+        //typeText(TASK_NUMBER_INPUT, "98765");
+        typeText(DESCRIPTION_INPUT, "Task created by automated test");
         clickElement(SAVE_BUTTON);
     }
 
