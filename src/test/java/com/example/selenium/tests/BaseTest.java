94ad0193540ac29ca91bc0536ceea7f22ef80014
BalazsBarkoczi <balazs.barkoczi121@gmail.com>
2026-05-11 07:20:41 +0200

    refactor: Simplify login process in CreateTaskTest by reusing BaseTest login method

--- src/test/java/com/example/selenium/tests/BaseTest.java
+++ src/test/java/com/example/selenium/tests/BaseTest.java
@@ -1,8 +1,22 @@
 package com.example.selenium.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
+
+import com.example.selenium.pages.LoginPage;
 
 public class BaseTest {
     protected WebDriver driver;
     protected static final String BASE_URL = "https://app.invoicing.co/#/";
+
+    protected void login(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigateToLoginPage();
+
+        String testEmail = System.getenv("TEST_EMAIL");
+        String testPassword = System.getenv("TEST_PASSWORD");
+
+        loginPage.loginWithEmailAndPassword(testEmail, testPassword);
+        Assertions.assertTrue(loginPage.waitForSuccessfulLogin(), "Login did not complete successfully");
+    }
 }
--- src/test/java/com/example/selenium/tests/CreateTaskTest.java
+++ src/test/java/com/example/selenium/tests/CreateTaskTest.java
@@ -7,22 +7,13 @@ import org.junit.jupiter.api.Test;
 
 import com.example.selenium.config.DriverConfig;
 import com.example.selenium.pages.CreateTaskPage;
-import com.example.selenium.pages.LoginPage;
 
 public class CreateTaskTest extends BaseTest {
 
     @BeforeEach
     public void setup() {
         driver = DriverConfig.initializeDriver();
-
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.navigateToLoginPage();
-
-        String testEmail = System.getenv("TEST_EMAIL");
-        String testPassword = System.getenv("TEST_PASSWORD");
-
-        loginPage.loginWithEmailAndPassword(testEmail, testPassword);
-        Assertions.assertTrue(loginPage.waitForSuccessfulLogin(), "Login did not complete successfully");
+        login();
     }
 
     @AfterEach
